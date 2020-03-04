1156df23931cab2a19e47fdc5cfc82946920f24a
CmdrNorthpaw <thetuxkid@gmail.com>
2020-03-04 15:20:52 +0000

    Updated fabric.mod.json with the mod's info.

--- src/main/kotlin/cmdrnorthpaw/stickysneak/ExampleMod.kt
+++ src/main/kotlin/cmdrnorthpaw/stickysneak/ExampleMod.kt
@@ -1,10 +1,41 @@
 package cmdrnorthpaw.stickysneak
 
+import io.github.cottonmc.cotton.config.ConfigManager
+import cmdrnorthpaw.stickysneak.StickySneakConfig
+import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding
+import net.fabricmc.fabric.api.event.client.ClientTickCallback
+import net.minecraft.client.MinecraftClient
+
 // For support join https://discord.gg/v6v4pMv
+
+var secondSneak = false
+var sneakTimer = 1
+var areTheyHolding = 1
 
 @Suppress("unused")
 fun init() {
-
-    println("Hello Fabric world!")
+    configInit = ConfigManager.loadConfig(StickySneakConfig.class)
 }
 
+ClientTickCallback.EVENT.Register(e -> {
+    if (areTheyHolding = 2 && MinecraftClient.getInstance().options.keySneak.isPressed()) {
+        areTheyHolding = 3
+    }
+    else {
+        areTheyHolding = 1
+    }
+
+    if (MinecraftClient.getInstance().options.keySneak.isPressed()) {
+        when (secondSneak){
+            true -> // Lock the sneak
+            false -> secondSneak = true
+        }
+        when (areTheyHolding) {
+            1 -> areTheyHolding = 2
+
+        }
+    }
+    println(areTheyHolding)
+
+
+})
--- src/main/kotlin/cmdrnorthpaw/stickysneak/config.kt
+++ src/main/kotlin/cmdrnorthpaw/stickysneak/config.kt
@@ -1,5 +1,11 @@
 package cmdrnorthpaw.stickysneak
 
-class config {
+import blue.endless.jankson.Comment
+import io.github.cottonmc.cotton.config.annotations.ConfigFile
+
+@ConfigFile(name=StickySneak)
+class StickySneakConfig {
+    @Comment(value= "Sets max tick time for sneak to lock down")
+     var maxTicksToLockSneak = 5
 
 }
